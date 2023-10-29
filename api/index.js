fc62bcc67c513e0784d4b8974b0a4b2486698d2c
Pankaj Kumar <122406409+pankajsmallarc@users.noreply.github.com>
2023-10-29 18:27:04 +0530

    creating router

--- api/index.js
+++ api/index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors'
 import mongoose from 'mongoose';
+import userRouter from './routes/userRoute.js'
 dotenv.config();
+const app = express();
 
 mongoose.connect(process.env.DATABASE).then(() => {
     console.log("connected to MOngoDB successfully".bgCyan)
 }).catch((err) => {
     console.warn(err)
 })
-
-const app = express();
+app.use('/api/v1',userRouter)
 app.listen(4000,()=>{
     console.log("Server is running at 4000".bgYellow)
 })
